954a5aedbc39c1f7fb7de5a3a71a0e2471f88296
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-11-10 10:50:27 -0600

    setting but not rendering

--- xammy/xammy/AnimalControl.xaml.cs
+++ xammy/xammy/AnimalControl.xaml.cs
@@ -26,6 +26,8 @@ namespace xammy
         }
 
         public string AnimalUri { get; private set; }
+        // public string AnimalUri = "https://www.dailypaws.com/thmb/nwNtJnMRSJ33nbdzrdsRwSJHMfU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/anime-cat-names-1204854078-2000-d34f509ae54943d8b78cfb4bf9ba0678.jpg";
+
         
         public AnimalControl()
         {
@@ -35,9 +37,14 @@ namespace xammy
         protected override void OnPropertyChanged(string propertyName = null)
         {
             base.OnPropertyChanged(propertyName);
-            if(propertyName == AnimalProperty.PropertyName)
+            if (propertyName == AnimalProperty.PropertyName && Animal == "Cat")
             {
                 this.AnimalUri = 
+                    "https://www.dailypaws.com/thmb/nwNtJnMRSJ33nbdzrdsRwSJHMfU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/anime-cat-names-1204854078-2000-d34f509ae54943d8b78cfb4bf9ba0678.jpg";
+            } else if (propertyName == AnimalProperty.PropertyName && Animal == "Frog")
+            {
+                this.AnimalUri =
+                    "https://i.etsystatic.com/isla/910f97/34229353/isla_fullxfull.34229353_lywq3bgq.jpg?version=0";
             }
         }
     }
